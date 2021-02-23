5bca80a9565f6d668f186d84ba39377d33506f6d
habeshawit <shalomseleshi@gmail.com>
2021-02-23 06:05:11 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// form and input fields
+const newTaskForm = document.getElementById("create-task-form");
+
+// ul for new task description
+const newList = document.getElementById('tasks');
+
+newTaskForm.addEventListener("submit", function(e) {
+  // stop form from submit
+  e.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  newList.appendChild(newTask);
+  
+  const newButton = document.createElement("button");
+  newButton.class = "delete-btn";
+  newButton.innerText = "X";
+  newTask.appendChild(newButton);
+  
+  // reset form
+  e.target.reset();
+})
+
+newList.addEventListener("click", function(event) {
+  if (event.target.class === "delete-btn") {
+    event.target.parentElement.remove();
+  }
+})
